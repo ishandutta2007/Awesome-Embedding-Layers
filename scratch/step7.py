477ace8053bab49eeb241fdb4a23da6f363cbdc7
Singularity Labs <ishandutta2007@gmail.com>
2026-07-05 07:25:53 +0530

    invalid awesome link fixed

---
+++ scratch/step7.py
@@ -0,0 +1,13 @@
+import re
+
+readme_path = r'C:\Users\ishan\Documents\Projects\Awesome-Embedding-Layers\README.md'
+
+with open(readme_path, 'r', encoding='utf-8') as f:
+    content = f.read()
+
+content = content.replace('https://github.com/sindresorhus/awesome', 'https://github.com/ishandutta2007/Awesome-Awesome-Awesome')
+
+with open(readme_path, 'w', encoding='utf-8') as f:
+    f.write(content)
+
+print("Step 7 completed")
